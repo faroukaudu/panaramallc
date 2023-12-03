c63d678e2b77a8f2d0d8a54d8f6f104692833961
Farouk Audu <fagzy99@gmail.com>
2023-12-03 01:50:58 +0100

    All update made
    Including responsitivity for mobile phone

--- index.js
+++ index.js
@@ -43,8 +43,8 @@ app.use(passport.session());
 
 
 //const Email = require('email').Email;
-const uri = "mongodb+srv://fancy98com:E6eoFBqkfDsweSKB@cluster0.rom3xsn.mongodb.net/flyboy";
-// const uri = "mongodb://127.0.0.1:27017/gitportalDB";
+// const uri = "mongodb+srv://fancy98com:E6eoFBqkfDsweSKB@cluster0.rom3xsn.mongodb.net/flyboy";
+const uri = "mongodb://127.0.0.1:27017/gitportalDB";
 
 
 database().catch(err => console.log(err));
